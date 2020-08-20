4a08e03d75a28a0e3873d556280dbcdf35779922
Adrian-14 <44221168+Adrian-14@users.noreply.github.com>
2020-08-19 22:26:45 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,79 +1,73 @@
 $(document).ready(function () {
+  let $btns = $(".project-area .button-group button");
 
+  $btns.click(function (e) {
+    $(".project-area .button-group button").removeClass("active");
+    e.target.classList.add("active");
 
-    let $btns = $('.project-area .button-group button');
-
-
-    $btns.click(function (e) {
-
-        $('.project-area .button-group button').removeClass('active');
-        e.target.classList.add('active');
-
-        let selector = $(e.target).attr('data-filter');
-        $('.project-area .grid').isotope({
-            filter: selector
-        });
-
-        return false;
-    })
-
-    $('.project-area .button-group #btn1').trigger('click');
-
-    $('.project-area .grid .test-popup-link').magnificPopup({
-        type: 'image',
-        gallery: { enabled: true }
+    let selector = $(e.target).attr("data-filter");
+    $(".project-area .grid").isotope({
+      filter: selector,
     });
 
+    return false;
+  });
 
-    // Owl-carousel
+  $(".project-area .button-group #btn1").trigger("click");
 
-    $('.site-main .about-area .owl-carousel').owlCarousel({
-        loop: true,
-        autoplay: true,
-        dots: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            560: {
-                items: 2
-            }
+  $(".project-area .grid .test-popup-link").magnificPopup({
+    type: "image",
+    gallery: { enabled: true },
+  });
+
+  // Owl-carousel
+
+  $(".site-main .about-area .owl-carousel").owlCarousel({
+    loop: true,
+    autoplay: true,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      560: {
+        items: 2,
+      },
+    },
+  });
+
+  // sticky navigation menu
+
+  let nav_offset_top = $(".header_area").height() + 50;
+
+  function navbarFixed() {
+    if ($(".header_area").length) {
+      $(window).scroll(function () {
+        let scroll = $(window).scrollTop();
+        if (scroll >= nav_offset_top) {
+          $(".header_area .main-menu").addClass("navbar_fixed");
+        } else {
+          $(".header_area .main-menu").removeClass("navbar_fixed");
         }
-    })
-
-    // sticky navigation menu
-
-    let nav_offset_top = $('.header_area').height() + 50;
-
-    function navbarFixed() {
-        if ($('.header_area').length) {
-            $(window).scroll(function () {
-                let scroll = $(window).scrollTop();
-                if (scroll >= nav_offset_top) {
-                    $('.header_area .main-menu').addClass('navbar_fixed');
-                } else {
-                    $('.header_area .main-menu').removeClass('navbar_fixed');
-                }
-            })
-        }
+      });
     }
+  }
 
-    navbarFixed();
-
+  navbarFixed();
 });
 
 //efecto escritura
 
-const typed = new Typed('.typed', {
-    stringsElement: '#cadena-texto',
-    typeSpeed: 75,
-    startDelay: 300,
-    backSpeed: 75,
-    smartBackspace: true,
-    shuffle: false,
-    backDelay: 1500,
-    loop: true,
-    loopCount: false,
-    showCursor: true,
-    contentType: 'html',
+const typed = new Typed(".typed", {
+  stringsElement: "#cadena-texto",
+  typeSpeed: 75,
+  startDelay: 300,
+  backSpeed: 75,
+  smartBackspace: true,
+  shuffle: false,
+  backDelay: 1500,
+  loop: true,
+  loopCount: false,
+  showCursor: true,
+  contentType: "html",
 });
