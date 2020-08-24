ee1c978df96b8bbf526efe360a35545bad91da25
Ghostman <ghostman@MacBook-Air-de-Ghostman.local>
2020-08-24 15:59:49 -0500

    update

--- js/main.js
+++ js/main.js
@@ -55,19 +55,3 @@ $(document).ready(function () {
 
   navbarFixed();
 });
-
-//efecto escritura
-
-const typed = new Typed(".typed", {
-  stringsElement: "#cadena-texto",
-  typeSpeed: 75,
-  startDelay: 300,
-  backSpeed: 75,
-  smartBackspace: true,
-  shuffle: false,
-  backDelay: 1500,
-  loop: true,
-  loopCount: false,
-  showCursor: true,
-  contentType: "html",
-});
