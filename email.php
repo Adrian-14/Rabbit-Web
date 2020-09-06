4a29e86036dac00eacb0055214e0d3bcd1e9d5b8
Ghostman <ghostman@MacBook-Air-de-Ghostman.local>
2020-09-05 19:23:36 -0500

    update

--- email.php
+++ email.php
@@ -1,13 +1,14 @@
 <?php 
     $destinatario = 'rabbitweb84@gmail.com';
-
+    $paquete = $_POST['paquete'];
     $nombre = $_POST['nombre'];
-    $destinatario = $_POST['destinatario'];
     $email = $_POST['email'];
     $msj = $_POST['msj'];
 
     $hrader = "Enviado desde el sitio web";
-    $mensajecompleto = $msj . "\nAtentamente: " + $nombre;
+    $msjcompleto = $msj . "\nAtentamente: " + $nombre;
 
-    
+    mail($destinatario, $email, $paquete, $nombre, $msjcompleto, $header);
+    echo "<script>alert('Correo enviado exitosamente')</script>";
+    echo "<script>setTimeout(\"location.href='index.html'\", 1000)</script>";
 ?>
